193858aa16203a259b66f535a9d28418067b75d5
Pinny Geliebter <pgibralter@gmail.com>
2021-07-21 13:48:36 -0400

    adds Secret.vue

---
+++ src/views/Secret.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="secret">
+    <h1>{{ message }}</h1>
+    <button>Don't click me</button>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the secret page",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
